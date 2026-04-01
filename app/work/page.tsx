47f334bd278a096576c5375bde9f1147bec0f033
Patrick Garcia <patrickgarcia@mbp-de-patrick.home>
2026-04-01 16:38:46 +0200

    description de ta modif

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -19,7 +19,7 @@ const projects = [
   {
     name: "Speedster",
     url: "https://speedster-sandy.vercel.app",
-    category: "Automobile",
+    category: "Chaussures de sport",
     description:
       "Un site audacieux et dynamique pour une marque de voitures de performance. Des visuels à fort contraste et une énergie cinétique qui reflète les voitures elles-mêmes.",
   },
